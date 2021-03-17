b250814de2510151b375a07aff8215d51490abb0
scottandrewp <70963325+scottandrewp@users.noreply.github.com>
2021-03-16 20:42:56 -0600

    ViewModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,17 +6,19 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using TempleTours.Models;
+using TempleTours.Models.ViewModels;
 
 namespace TempleTours.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        
+        private ReservationsDbContext Context;
         
         public HomeController(ILogger<HomeController> logger, ReservationsDbContext con)
         {
             _logger = logger;
+            Context = con;
         }
 
         //Index Action - no models needed?
@@ -29,7 +31,27 @@ namespace TempleTours.Controllers
         [HttpGet("SignUp")]
         public IActionResult SignUp()
         {
-            return View();
+            return View(new DayOfWeekViewModel 
+            {
+                Monday = Context.TourSlots
+                    .Where(x => x.DayOfWeek == "Monday" && x.Available == true),
+
+                Tuesday = Context.TourSlots
+                    .Where(x => x.DayOfWeek == "Tuesday" && x.Available == true),
+
+                Wednesday = Context.TourSlots
+                    .Where(x => x.DayOfWeek == "Wednesday" && x.Available == true),
+
+                Thursday = Context.TourSlots
+                    .Where(x => x.DayOfWeek == "Thursday" && x.Available == true),
+
+                Friday = Context.TourSlots
+                    .Where(x => x.DayOfWeek == "Friday" && x.Available == true),
+
+                Saturday = Context.TourSlots
+                    .Where(x => x.DayOfWeek == "Saturday" && x.Available == true)
+
+            });
         }
 
         //Sign Up POST Action 
---
+++ Models/ViewModels/DayOfWeekViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleTours.Models.ViewModels
+{
+    public class DayOfWeekViewModel
+    {
+        public IEnumerable<TourSlot> Monday { get; set; }
+        public IEnumerable<TourSlot> Tuesday { get; set; }
+        public IEnumerable<TourSlot> Wednesday { get; set; }
+        public IEnumerable<TourSlot> Thursday { get; set; }
+        public IEnumerable<TourSlot> Friday { get; set; }
+        public IEnumerable<TourSlot> Saturday { get; set; }
+    }
+}
